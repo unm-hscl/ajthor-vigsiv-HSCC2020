a4ea757e11b7e6a7b7adcd2e3b0faebe43d248c7
vigsiv <vigsiv@unm.edu>
2019-10-19 21:55:52 -0600

    Added input generator for planar quadcopter and in prog. revisions for quad_samples

--- generate_samples_quad.m
+++ generate_samples_quad.m
@@ -1,25 +1,7 @@
-function varargout = generate_samples_quad(m,params,varargin)
-% GENERATE_SAMPLES_INT2D Generate samples for a 2-D integrator.
-%
-%   GENERATE_SAMPLES_INT2D(m) Generates m samples for a 2-D integrator.
-%
-%   GENERATE_SAMPLES_INT2D parameters:
-%
-%   SampleVariance      - The variance on the initial condition x_0.
-%   DisturbanceVariance - The variance of the disturbance w_0.
-%   DisturbanceMatrix   - The real-valued matrix G in the system equations.
-%
-%   x[k+1] = A*x[k] + G*w[k]
-
-% Load the state matrix.
-% data = load('cwh.mat');
+function varargout = generate_samples_quad(params,varargin)
 
 rng(0);
 
-% % Plot on?
-% plot_on = true;
-
-n_copters = params.n_copters;
 N = params.N;
 Xmin = params.Xmin;
 Xmax = params.Xmax;
@@ -33,14 +15,15 @@ Tmin = params.Tmin;
 Tmax = params.Tmax;
 dTmin = params.dTmin;
 dTmax = params.dTmax;
-
 Umin = params.Umin;
 Umax = params.Umax;
+disturb = params.disturb; 
 
-% Each element in this vector is the number of samples along each
-% dimension. So the first element corresponds to X, the second dX, etc.
-% Total number of samples is the product of this vector.
-el = [10, 3, 10, 3, 3, 3];
+% el  = [10, 2, 10, 2, 4, 2]; 
+el = params.el;
+
+
+
 fprintf('Number of elements: %d\n', prod(el));
 
 X = linspace(Xmin, Xmax, el(1));
@@ -67,75 +50,6 @@ U = [5; 5];
 Xs = [X; dX; Y; dY; T; dT];
 Us = U;
 
-% Covariance matrix. 
-% Each element along the diagonal is the disturbance of a particular
-% variable. I arbitrarily set the disturbance on X/Y/T to 1E-3.
-S = [ 1E-3, 5E-8, 1E-3, 5E-8, 1E-5, 5E-8];
-S = repmat(S, 1, n_copters);
-
-Sigma = sparse(1:n_copters*6, 1:n_copters*6, S);
-
-% if plot_on
-%     % Show the nonzero elements of the covariance matrix.
-%     figure
-%     spy(Sigma);
-% end
-
-
-% Repeat for however many copters you have.
-Xs = repmat(Xs, n_copters, 1);
-Us = repmat(Us, n_copters, 1);
-
-% Shift the X values for all copters.
-for k = 7:6:n_copters*6
-    Xs(k, :) = Xs(k-6, :) + Xmax;
-end
-
-%% Generate system matrices.
-
-m = 5; 
-r = 2;
-I = 2;
-g = 9.81;
-Ts = 0.25;
-
-A = [0 1 0 0   0 0;
-     0 0 0 0   0 0;
-     0 0 0 1   0 0;
-     0 0 0 0   0 0;
-     0 0 0 0   0 1;
-     0 0 0 0   0 0;];
- 
-B = [0     0;
-     0     0;
-     0     0;
-     1/m 1/m;
-     0     0;
-     r/I -r/I;];
- 
-sys = ss(A,B,eye(1,size(A,2)),0);
-
-sysd = c2d(sys,Ts);
-
-A = sysd.A;
-B = sysd.B;
-         
-A = kron(speye(n_copters),A);
-
-
-% if plot_on
-%     % Show the nonzero elements of the A matrix.
-%     figure
-%     spy(A);
-% end
-
-B = kron(speye(n_copters),B);
-
-% if plot_on
-%     % Show the nonzero elements of the B matrix.
-%     figure
-%     spy(B);
-% end
 
 %% Generate output samples.
 
@@ -143,18 +57,21 @@ for i = 1:N
     
     if i == 1
 
-        Ys(1,:) = Xs(1,:) + Ts*Xs(2,:) + S(1)*randn(1, prod(el));
-        Ys(2,:) = -Ts/m*sin(Xs(3,:))*(U(1)+U(2)) + Xs(2,:) + S(2)*randn(1, prod(el));
-        Ys(3,:) = Xs(3,:) + Ts*Xs(4,:)+ S(3)*randn(1, prod(el));
-        Ys(4,:) = Ts/m*cos(Xs(3,:))*(U(1)+U(2)) - Ts/g + Xs(4,:) + S(4)*randn(1, prod(el));
-        Ys(5,:) = Xs(5,:) + Ts*Xs(6,:)+ S(5)*randn(1, prod(el));
-        Ys(6,:) = Ts*r/I*(U(1)-U(2)) + Xs(6,:) + S(6)*randn(1, prod(el));
-        
-        
-        
+        Ys(1,:) = Xs(1,:) + Ts*Xs(2,:) + disturb(1);
+        Ys(2,:) = -Ts/m*sin(Xs(3,:))*(U(1)+U(2)) + Xs(2,:) + disturb(2);
+        Ys(3,:) = Xs(3,:) + Ts*Xs(4,:)+ disturb(3);
+        Ys(4,:) = Ts/m*cos(Xs(3,:))*(U(1)+U(2)) - Ts/g + Xs(4,:) + disturb(4);
+        Ys(5,:) = Xs(5,:) + Ts*Xs(6,:) + disturb(5);
+        Ys(6,:) = Ts*r/I*(U(1)-U(2)) + Xs(6,:) + disturb(6);
+
     else
         Xs = Ys;
-        Ys = A*Xs + B*Us + Sigma*randn(6*n_copters, prod(el));
+        Ys(1,:) = Xs(1,:) + Ts*Xs(2,:) + disturb(1);
+        Ys(2,:) = -Ts/m*sin(Xs(3,:))*(U(1)+U(2)) + Xs(2,:) + disturb(2);
+        Ys(3,:) = Xs(3,:) + Ts*Xs(4,:)+ disturb(3);
+        Ys(4,:) = Ts/m*cos(Xs(3,:))*(U(1)+U(2)) - Ts/g + Xs(4,:) + disturb(4);
+        Ys(5,:) = Xs(5,:) + Ts*Xs(6,:) + disturb(5);
+        Ys(6,:) = Ts*r/I*(U(1)-U(2)) + Xs(6,:) + disturb(6);
         
     end
     
@@ -163,7 +80,6 @@ end
 
 switch nargout
 case 1
-  % varargout{1} = [Xs; Ys; dXs; dYs];
   varargout{1} = Ys;
 case 2
   varargout{1} = Ys;
