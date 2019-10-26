329f7c138285a3e6488fe393fac6fca1e925bca1
adam <adam.thorpe.g@gmail.com>
2019-10-26 15:45:50 -0600

    Updating code comments.

--- code/algorithms/@KernelDistributionEmbeddings/ComputeSafetyProbabilities.m
+++ code/algorithms/@KernelDistributionEmbeddings/ComputeSafetyProbabilities.m
@@ -22,16 +22,19 @@ validateSamples = ...
 validateTestPoints = ...
     @(arg) validateattributes(arg, {'numeric'}, {'nrows', samples.n});
 
+% Add parameters that are passed to the function.
 addRequired(p, 'problem', validateProblem);
 addRequired(p, 'samples', validateSamples);
 addRequired(p, 'Xtest', validateTestPoints);
 addOptional(p, 'Utest', zeros(samples.m, size(Xtest, 2)));
 
+% Parse the parameters. These are stored in the p.Results struct after parsing.
 parse(p, problem, samples, Xtest, varargin{:});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Define Constants
 
+% Utest are the inputs corresponding to the Xtest evaluation points.
 Utest = p.Results.Utest;
 
 % The final time for the problem.
@@ -42,39 +45,54 @@ X = samples.StateSamplesX;
 U = samples.InputSamplesU;
 Y = samples.StateSamplesY;
 
-
+% The number of samples.
 M  = size(X, 2);
+% The number of evaluation points.
 Mt = size(Xtest, 2);
-
-t_start = tic;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute weight matrix.
 
+% The Gram matrix for the state samples.
 Gx = algorithm.ComputeKernel(X, X);
+% The Gram matrix for the input samples.
 Gu = algorithm.ComputeKernel(U, U);
 
+% The composite Gram matrix.
 G = Gx.*Gu;
 
+% In order to ease computation, we formulate an intermediary 'weight' matrix,
+% which is inverted here once, instead of inverting twice later.
 W = inv(G + algorithm.Lambda*M*eye(M));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute value functions.
 
+% The value function evaluations at all of the test points.
 Vk = zeros(N, M);
 
+% We compute the kernel function between the state input samples and the output
+% samples, and the control input samples and itself.
 cxy = algorithm.ComputeKernel(X, Y);
 cuv = algorithm.ComputeKernel(U, U);
+
+% Then, we compute the coefficients for each sample point.
 beta = cxy.*cuv;
 beta = W*beta; %#ok<*MINV>
+
+% And normalize the coefficient vector.
 beta = beta./sum(abs(beta), 1);
 
 switch class(problem)
 
     case 'FirstHittingTimeProblem'
 
+        % We compute the terminal value function using the target set. This
+        % function is known, while the others need to be estimated.
         Vk(N, :) = problem.TargetSet(Y);
 
+        % For the entire time horizon from [1, N-1] (using the canonical
+        % zero-indexing), we compute the value functions for the sample points.
         for k = N-1:-1:2
             Vk(k, :) = problem.TargetSet(Y) + ...
                        (problem.ConstraintSet(Y) & ...
@@ -83,8 +101,12 @@ switch class(problem)
 
     case 'TerminalHittingTimeProblem'
 
+        % We compute the terminal value function using the target set. This
+        % function is known, while the others need to be estimated.
         Vk(N, :) = problem.TargetSet(Y);
 
+        % For the entire time horizon from [1, N-1] (using the canonical
+        % zero-indexing), we compute the value functions for the sample points.
         for k = N-1:-1:2
             Vk(k, :) = problem.ConstraintSet(Y).*(Vk(k+1, :)*beta);
         end
@@ -94,20 +116,31 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute probabilities for point.
 
+% Create a matrix to hold the probability values computed using the algorithm.
 Pr = zeros(N, Mt);
 
+% Compute the kernel function between the state input samples and the state test
+% points, and the control input samples and the input test points.
 cxt = algorithm.ComputeKernel(X, Xtest);
 cut = algorithm.ComputeKernel(U, Utest);
+% Then, we compute the coefficients for each test point.
 beta = cxt.*cut;
 beta = W*beta;
+
+% And normalize the coefficient vector.
 beta = beta./sum(abs(beta), 1);
 
 switch class(problem)
 
     case 'FirstHittingTimeProblem'
 
+        % We compute the terminal probabilities using the target set. This
+        % function is known, while the others need to be estimated.
         Pr(N, :) = problem.TargetSet(Xtest);
 
+        % For the entire time horizon from [0, N-1] (using the canonical
+        % zero-indexing), we compute the safety probabilities for the test
+        % points.
         for k = N-1:-1:1
             Pr(k, :) = problem.TargetSet(Xtest) + ...
                        (problem.ConstraintSet(Xtest) & ...
@@ -116,12 +149,19 @@ switch class(problem)
 
     case 'TerminalHittingTimeProblem'
 
+        % We compute the terminal probabilities using the target set. This
+        % function is known, while the others need to be estimated.
         Pr(N, :) = problem.TargetSet(Xtest);
 
+        % For the entire time horizon from [0, N-1] (using the canonical
+        % zero-indexing), we compute the safety probabilities for the test
+        % points.
         for k = N-1:-1:1
             Pr(k, :) = problem.ConstraintSet(Xtest).*(Vk(k+1, :)*beta);
         end
 
 end
+
+% The probabilities Pr are subsequently returned from the function.
 
 end
--- code/algorithms/@KernelDistributionEmbeddingsRFF/ComputeSafetyProbabilities.m
+++ code/algorithms/@KernelDistributionEmbeddingsRFF/ComputeSafetyProbabilities.m
@@ -32,6 +32,7 @@ parse(p, problem, samples, Xtest, varargin{:});
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Define Constants
 
+% Utest are the inputs corresponding to the Xtest evaluation points.
 Utest = p.Results.Utest;
 
 % The final time for the problem.
@@ -42,32 +43,40 @@ X = samples.StateSamplesX;
 U = samples.InputSamplesU;
 Y = samples.StateSamplesY;
 
-
+% The number of samples.
 M  = size(X, 2);
+% The number of evaluation points.
 Mt = size(Xtest, 2);
-
-t_start = tic;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute random fourier features.
+
+% We start with samples from the Gaussian distribution, which is the Fourier
+% transform of the Gaussian kernel.
 wx = (1/algorithm.Sigma)*randn(algorithm.D, size(X, 1));
 wu = (1/algorithm.Sigma)*randn(algorithm.D, size(U, 1));
 
+% We also compute uniform samples from [-2pi 2pi].
 bx = (2*pi)*rand(algorithm.D, 1);
 bu = (2*pi)*rand(algorithm.D, 1);
 
+% Then, we compute the random Fourier features, which are cosines scaled by a
+% constant value.
 Zx = (sqrt(2)/sqrt(algorithm.D))*cos(wx*X + bx);
 Zu = (sqrt(2)/sqrt(algorithm.D))*cos(wu*U + bu);
 Zy = (sqrt(2)/sqrt(algorithm.D))*cos(wx*Y + bx);
 
+% Compute the composite feature vector.
 Z = Zx;
 
-H = Z*Z.';
-W = H + algorithm.Lambda*M*eye(algorithm.D); %#ok<*MINV>
+% We then compute the feture matrix and the coefficient vector using the
+% random Fourier features.
+W = Z*Z.' + algorithm.Lambda*M*eye(algorithm.D); %#ok<*MINV>
 
 beta = W\Z;
 gamma = beta.'*Zy;
 
+% And normalize the coefficient vector.
 gamma = gamma./sum(gamma, 1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -78,6 +87,8 @@ switch class(problem)
 
     case 'FirstHittingTimeProblem'
 
+        % We compute the terminal value function using the target set. This
+        % function is known, while the others need to be estimated.
         Vk(N, :) = problem.TargetSet(Y);
 
         for k = N-1:-1:2
@@ -89,6 +100,8 @@ switch class(problem)
 
     case 'TerminalHittingTimeProblem'
 
+        % We compute the terminal value function using the target set. This
+        % function is known, while the others need to be estimated.
         Vk(N, :) = problem.TargetSet(Y);
 
         for k = N-1:-1:2
@@ -100,25 +113,32 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute probabilities for point.
+
+% Create a matrix to hold the probability values computed using the algorithm.
 Pr = zeros(N, Mt);
 
-% wXtest = (1/algorithm.Sigma)*randn(algorithm.D, size(Xtest, 1));
-%
-% bXtest = (2*pi)*rand(algorithm.D, 1);
-
+% Compute the random Fourier features for the state test points and the input
+% test points.
 ZXtest = (sqrt(2)/sqrt(algorithm.D))*cos(wx*Xtest + bx);
 ZUtest = (sqrt(2)/sqrt(algorithm.D))*cos(wu*Utest + bu);
 
+% Then, we compute the coefficient vector for each test point.
 gamma = beta.'*ZXtest;
 
+% And normalize the coefficient vector.
 gamma = gamma./sum(gamma, 1);
 
 switch class(problem)
 
     case 'FirstHittingTimeProblem'
 
+        % We compute the terminal probabilities using the target set. This
+        % function is known, while the others need to be estimated.
         Pr(N, :) = problem.TargetSet(Xtest);
 
+        % For the entire time horizon from [0, N-1] (using the canonical
+        % zero-indexing), we compute the safety probabilities for the test
+        % points.
         for k = N-1:-1:1
             Pr(k, :) = problem.TargetSet(Xtest) + ...
                        (problem.ConstraintSet(Xtest) & ...
@@ -128,13 +148,20 @@ switch class(problem)
 
     case 'TerminalHittingTimeProblem'
 
+        % We compute the terminal probabilities using the target set. This
+        % function is known, while the others need to be estimated.
         Pr(N, :) = problem.TargetSet(Xtest);
 
+        % For the entire time horizon from [0, N-1] (using the canonical
+        % zero-indexing), we compute the safety probabilities for the test
+        % points.
         for k = N-1:-1:1
             Pr(k, :) = problem.ConstraintSet(Xtest).* ...
                 (Vk(k+1, :)*gamma);
         end
 
 end
+
+% The probabilities Pr are subsequently returned from the function.
 
 end
